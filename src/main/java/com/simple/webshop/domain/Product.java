8f524dccb58c22464178400e1355cf7c21ea41b4
c.misman <mismancelaleddin@gmail.com>
2023-07-27 06:23:49 +0300

    Product model added

---
+++ src/main/java/com/simple/webshop/domain/Product.java
@@ -0,0 +1,27 @@
+package com.simple.webshop.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_seq")
+    @SequenceGenerator(name = "product_id_seq", sequenceName = "product_id_seq")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @ManyToOne
+    private Brand brand;
+}
