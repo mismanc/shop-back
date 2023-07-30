2894cdd202b8dd66fe39bb54ff7c5c191358e0ee
c.misman <mismancelaleddin@gmail.com>
2023-07-30 23:59:23 +0300

    bugs fixed

--- src/main/java/com/simple/webshop/domain/Order.java
+++ src/main/java/com/simple/webshop/domain/Order.java
@@ -2,6 +2,7 @@ package com.simple.webshop.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
 
@@ -15,7 +16,15 @@ public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_id_gen")
-    @SequenceGenerator(name = "order_id_gen", sequenceName = "order_id_seq")
+    @GenericGenerator(
+            name = "order_id_gen",
+            type = org.hibernate.id.enhanced.SequenceStyleGenerator.class,
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
+                    @org.hibernate.annotations.Parameter(name = "increment_size", value = "5"),
+                    @org.hibernate.annotations.Parameter(name = "optimizer", value = "pooled")
+            }
+    )
     private Long id;
 
     @Column(nullable = false, scale = 2, precision = 10)
